243bbc349827411e53cd95d251e287080f69941b
Cameron Slupeiks <cslupeiks@gmail.com>
2019-04-12 15:37:23 -0400

    Website Update

--- src/components/SkillsPage.jsx
+++ src/components/SkillsPage.jsx
@@ -16,11 +16,10 @@ class SkillsPage extends Component {
                 <i className="devicon-javascript-plain" />
                 <i className="devicon-java-plain" />
                 <i className="devicon-python-plain" />
-                <i className="devicon-ruby-plain" />
                 <i className="devicon-go-plain" />
+                <i className="devicon-ruby-plain" />
                 <i className="devicon-swift-plain" />
                 <i className="devicon-c-plain" />
-                <i className="devicon-csharp-plain" />
                 <i className="devicon-cplusplus-plain" />
               </div>
             </div>
@@ -28,9 +27,8 @@ class SkillsPage extends Component {
               <b>FRONT-END</b>
               <div className="icon-container">
                 <i className="devicon-react-original" />
-                <i className="devicon-vuejs-plain" />
                 <i className="devicon-angularjs-plain" />
-                <i className="devicon-jquery-plain" />
+                <i className="devicon-vuejs-plain" />
                 <i className="devicon-bootstrap-plain" /> 
                 <i className="devicon-html5-plain" />
                 <i className="devicon-css3-plain" /> 
@@ -40,24 +38,20 @@ class SkillsPage extends Component {
               <b>BACK-END</b>
               <div className="icon-container">
                 <i className="devicon-nodejs-plain" />
-                <i className="devicon-rails-plain" />
                 <i className="devicon-django-plain" />
-                <i className="devicon-docker-plain" />
+                <i className="devicon-rails-plain" />
                 <i className="devicon-mongodb-plain" />
                 <i className="devicon-mysql-plain" />
-                <i className="devicon-amazonwebservices-original" />
               </div>
             </div>
             <div className="Tools">
               <b>TOOLS</b>
               <div className="icon-container">
                 <i className="devicon-git-plain" />
-                <i className="devicon-github-plain" />
-                <i className="devicon-gitlab-plain" />
-                <i className="devicon-jasmine-plain" />
-                <i className="fab fa-npm" />
+                <i className="devicon-docker-plain" />
+                <i className="devicon-kubernetes-plain" />
+                <i className="devicon-amazonwebservices-original" />
                 <i className="devicon-visualstudio-plain" />
-                <i className="devicon-vim-plain" />
               </div>
             </div>
           </div>
@@ -71,29 +65,25 @@ export default withScrollReveal([
   {
     selector: '.Languages',
     options: {
-      duration: 2500,
-      delay: 200,
+      duration: 2000
     }
   },
   {
     selector: '.Frontend',
     options: {
-      duration: 2500,
-      delay: 400,
+      duration: 2000
     }
   },
   {
     selector: '.Backend',
     options: {
-      duration: 2500,
-      delay: 600,
+      duration: 2000
     }
   },
   {
     selector: '.Tools',
     options: {
-      duration: 2500,
-      delay: 800,
+      duration: 2000
     }
   }
 ]) (SkillsPage);
